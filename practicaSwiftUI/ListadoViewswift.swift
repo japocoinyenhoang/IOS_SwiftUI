f82e3e09b44a1311019d193d966060d2c74a0bbb
japocoinyenhoang <japocoin@gmail.com>
2021-05-25 17:24:15 +0200

    change from individual to array of programadores

--- practicaSwiftUI/ListadoViewswift.swift
+++ practicaSwiftUI/ListadoViewswift.swift
@@ -7,10 +7,16 @@
 
 import SwiftUI
 
+private let programadores = [
+    Programador(id: 0, nombre: "Jorge", lenguajes: "Java C++", avatar: Image(systemName: "person.circle.fill")),
+    Programador(id: 1, nombre: "Sara", lenguajes: "JavaScript Kotlin", avatar: Image(systemName: "person.circle.fill")),
+    Programador(id: 2, nombre: "Paco", lenguajes: "C# C++ Unity", avatar: Image(systemName: "person.circle.fill")),
+    Programador(id: 3, nombre: "Yen", lenguajes: "JavaScript kotlin", avatar: Image(systemName: "person.circle.fill"))]
+
 struct ListadoViewswift: View {
     var body: some View {
-        List {
-            CeldaView(programador: Programador(id: 0, nombre: "Jorge", lenguajes: "Java C++", avatar: Image(systemName: "person.circle.fill")))
+        List(programadores, id: \.id) { programador in
+            CeldaView(programador: programador)
         }
     }
 }
